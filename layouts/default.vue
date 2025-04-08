5bf8a4b530c24bc807e69171fe91adaf35473ed6
NRaykov <nediotenev@gmail.com>
2025-04-08 17:26:02 +0300

    Create navbar.vue

---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import Navbar from '~/components/layouts/navbar.vue';
+</script>
+<template>
+    <div class="h-screen">
+        <Navbar />
+        <div class="flex-1 overflow-auto">
+            <slot />
+        </div>
+    </div>
+</template>
